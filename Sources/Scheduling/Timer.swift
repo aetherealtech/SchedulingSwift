6feecd0c6c7bdca4f97a7e92d7cefc90e42191ec
Daniel Coleman <dcoleman0711@gmail.com>
2022-01-12 19:02:28 -0600

    Make Timer generic and able to generate values

--- Sources/Scheduling/Timer.swift
+++ Sources/Scheduling/Timer.swift
@@ -5,48 +5,124 @@
 import Foundation
 
 public class Timer {
-    
-    public static func schedule<FireTimes: Sequence>(
-        at fireTimes: FireTimes,
-        on scheduler: Scheduler,
-        _ work: @escaping () -> Void
-    ) -> Timer where FireTimes.Element == Date {
-        
+
+    public static func scheduleWithValues<Value, Values: Sequence>(
+        values: Values,
+        getFireTime: @escaping (Value) -> Date,
+        scheduler: Scheduler,
+        _ work: @escaping (Value) -> Void
+    ) -> Timer where Values.Element == Value {
+
         Timer(
-            fireTimes: fireTimes,
+            values: values,
+            getFireTime: getFireTime,
+            scheduler: scheduler,
+            work: work
+        )
+    }
+
+    private init<Value, Values: Sequence>(
+        values: Values,
+        getFireTime: @escaping (Value) -> Date,
+        scheduler: Scheduler,
+        work: @escaping (Value) -> Void
+    ) where Values.Element == Value {
+
+        self.imp = TimerWithValue(
+            values: values,
+            getFireTime: getFireTime,
             scheduler: scheduler,
             work: work
         )
     }
     
-    private init<FireTimes: Sequence>(
-        fireTimes: FireTimes,
+    private let imp: TimerImp
+}
+
+extension Scheduler {
+
+    public func runTimer<FireTimes: Sequence>(
+        at fireTimes: FireTimes,
+        _ work: @escaping () -> Void
+    ) -> Timer where FireTimes.Element == Date {
+
+        runTimer(
+            values: fireTimes,
+            getFireTime: { fireTime in fireTime }
+        ) { _ in
+
+            work()
+        }
+    }
+
+    public func runTimer<Value, Values: Sequence>(
+        values: Values,
+        _ work: @escaping (Value) -> Void
+    ) -> Timer where Values.Element == (Value, Date) {
+
+        runTimer(
+            values: values,
+            getFireTime: { (value, time) in time }
+        ) { (value, _) in
+
+            work(value)
+        }
+    }
+
+    public func runTimer<Value, Values: Sequence>(
+        values: Values,
+        getFireTime: @escaping (Value) -> Date,
+        work: @escaping (Value) -> Void
+    ) -> Timer where Values.Element == Value {
+
+        Timer.scheduleWithValues(
+            values: values,
+            getFireTime: getFireTime,
+            scheduler: self,
+            work
+        )
+    }
+}
+
+private protocol TimerImp {
+
+}
+
+private class TimerWithValue<Value> : TimerImp {
+
+    init<Values: Sequence>(
+        values: Values,
+        getFireTime: @escaping (Value) -> Date,
         scheduler: Scheduler,
-        work: @escaping () -> Void
-    ) where FireTimes.Element == Date {
-        
-        self.nextFireTime = AnyIterator(fireTimes.makeIterator())
+        work: @escaping (Value) -> Void
+    ) where Values.Element == Value {
+
+        self.valueIterator = AnyIterator(values.makeIterator())
+        self.getFireTime = getFireTime
+
         self.work = work
         self.scheduler = scheduler
-        
+
         scheduleNext()
     }
-    
-    private let nextFireTime: AnyIterator<Date>
-    private let work: () -> Void
+
+    private let valueIterator: AnyIterator<Value>
+    private let getFireTime: (Value) -> Date
+
+    private let work: (Value) -> Void
     private let scheduler: Scheduler
-    
+
     private func scheduleNext() {
-        
-        guard let fireTime = nextFireTime.next() else {
+
+        guard let nextValue = valueIterator.next() else {
             return
         }
-        
-        scheduler.runAt(time: fireTime, { [weak self] in
-            
+
+        scheduler.runAt(time: getFireTime(nextValue), { [weak self] in
+
             guard let strongSelf = self else { return }
-            
-            strongSelf.work()
+
+            strongSelf.work(nextValue)
             strongSelf.scheduleNext()
         })
     }
--- Tests/SchedulingTests/TimerTests.swift
+++ Tests/SchedulingTests/TimerTests.swift
@@ -41,9 +41,8 @@ final class TimerTests: XCTestCase {
 
             let scheduler = MockScheduler()
 
-            let timer = Timer.schedule(
-                at: fireDates,
-                on: scheduler
+            let timer = scheduler.runTimer(
+                at: fireDates
             ) {
 
                 workInvocations += 1
@@ -77,9 +76,8 @@ final class TimerTests: XCTestCase {
 
             var timer: Scheduling.Timer! = nil
 
-            timer = Timer.schedule(
-                at: fireDates,
-                on: scheduler
+            timer = scheduler.runTimer(
+                at: fireDates
             ) {
                 
                 if workInvocations == invocationsCount {
@@ -93,6 +91,49 @@ final class TimerTests: XCTestCase {
             scheduler.process()
 
             XCTAssertEqual(workInvocations, invocationsCount)
+        }
+    }
+
+    struct TestTimerValue : Equatable {
+
+        let data: String
+        let fireTime: Date
+    }
+
+    func testWithValues() {
+
+        for _ in 0..<100 {
+
+            let values = (0..<10).map { index in
+
+                TestTimerValue(
+                    data: "Value #\(index)",
+                    fireTime: Date() + 8.5 * Double(index)
+                )
+            }
+
+            let fireDates = values.map { value in value.fireTime }
+
+            var receivedValues = [TestTimerValue]()
+
+            let scheduler = MockScheduler()
+
+            let timer = scheduler.runTimer(
+                values: values,
+                getFireTime: { value in value.fireTime }
+            ) { value in
+
+                receivedValues.append(value)
+            }
+
+            scheduler.process()
+
+            XCTAssertTrue(scheduler.runAtInvocations.elementsEqual(fireDates, by: { invocation, expectedFireTime in
+
+                invocation.time == expectedFireTime
+            }))
+
+            XCTAssertEqual(receivedValues, values)
         }
     }
 }
